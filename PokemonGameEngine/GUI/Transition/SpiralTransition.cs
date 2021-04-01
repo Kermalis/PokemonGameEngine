29203b1b4f63b3e2f10a18d91870a18c2395e878
Kermalis <29823718+Kermalis@users.noreply.github.com>
2021-04-01 04:15:13 -0400

    Fix SpiralTransition not drawing all boxes

--- PokemonGameEngine/GUI/Transition/SpiralTransition.cs
+++ PokemonGameEngine/GUI/Transition/SpiralTransition.cs
@@ -9,99 +9,107 @@ namespace Kermalis.PokemonGameEngine.GUI.Transition
         // In the future we can have it draw bigger squares if it's not cleanly divisible (like, outside the bounds of the screen)
         private const int NumBoxes = 8;
 
-        private int _counterX;
-        private int _counterY;
-        private int _target;
-        private int _numCompleted;
-        private bool doX;
-        private bool goBackwards;
+        private int _counter;
         private Action _onTransitionEnded;
 
         public SpiralTransition(Action onTransitionEnded)
         {
             _onTransitionEnded = onTransitionEnded;
-            doX = true;
-            _target = NumBoxes - 1;
+        }
+
+        private unsafe void SpiralTransitionLogic(uint* bmpAddress, int bmpWidth, int bmpHeight, int num)
+        {
+            int counterX = 0;
+            int counterY = 0;
+            int target = NumBoxes - 1;
+            int numCompleted = 0;
+            bool doX = true;
+            bool goBackwards = false;
+
+            int boxWidth = bmpWidth / NumBoxes;
+            int boxHeight = bmpHeight / NumBoxes;
+            for (int i = 0; i <= num; i++)
+            {
+                // Draw
+                RenderUtils.FillRectangle(bmpAddress, bmpWidth, bmpHeight, counterX * boxWidth, counterY * boxHeight, boxWidth, boxHeight, RenderUtils.Color(0, 0, 0, 255));
+
+                // If it is done (we want to draw the final box before we say this is done)
+                if (doX
+                    ? goBackwards ? counterX < target : counterX > target
+                    : goBackwards ? counterY < target : counterY > target)
+                {
+                    _onTransitionEnded.Invoke();
+                    _onTransitionEnded = null;
+                    return;
+                }
+
+                // Update the spiral
+                if (doX)
+                {
+                    if (goBackwards)
+                    {
+                        if (counterX == target)
+                        {
+                            doX = false;
+                            counterY--;
+                            target++;
+                        }
+                        else
+                        {
+                            counterX--;
+                        }
+                    }
+                    else
+                    {
+                        if (counterX == target)
+                        {
+                            doX = false;
+                            counterY++;
+                        }
+                        else
+                        {
+                            counterX++;
+                        }
+                    }
+                }
+                else
+                {
+                    if (goBackwards)
+                    {
+                        if (counterY == target)
+                        {
+                            doX = true;
+                            goBackwards = false;
+                            counterX++;
+                            numCompleted++;
+                            target = NumBoxes - 1 - numCompleted;
+                        }
+                        else
+                        {
+                            counterY--;
+                        }
+                    }
+                    else
+                    {
+                        if (counterY == target)
+                        {
+                            doX = true;
+                            goBackwards = true;
+                            counterX--;
+                            target = numCompleted;
+                        }
+                        else
+                        {
+                            counterY++;
+                        }
+                    }
+                }
+            }
         }
 
         public unsafe void RenderTick(uint* bmpAddress, int bmpWidth, int bmpHeight)
         {
-            // Draw current spiral
-            int boxWidth = bmpWidth / NumBoxes;
-            int boxHeight = bmpHeight / NumBoxes;
-            RenderUtils.FillRectangle(bmpAddress, bmpWidth, bmpHeight, _counterX * boxWidth, _counterY * boxHeight, boxWidth, boxHeight, RenderUtils.Color(0, 0, 0, 255));
-
-            // If it is done (we want to draw the final box before we say this is done)
-            if (doX
-                ? goBackwards ? _counterX < _target : _counterX > _target
-                : goBackwards ? _counterY < _target : _counterY > _target)
-            {
-                _onTransitionEnded.Invoke();
-                _onTransitionEnded = null;
-                return;
-            }
-
-            // Update the spiral
-            if (doX)
-            {
-                if (goBackwards)
-                {
-                    if (_counterX == _target)
-                    {
-                        doX = false;
-                        _counterY--;
-                        _target++;
-                    }
-                    else
-                    {
-                        _counterX--;
-                    }
-                }
-                else
-                {
-                    if (_counterX == _target)
-                    {
-                        doX = false;
-                        _counterY++;
-                    }
-                    else
-                    {
-                        _counterX++;
-                    }
-                }
-            }
-            else
-            {
-                if (goBackwards)
-                {
-                    if (_counterY == _target)
-                    {
-                        doX = true;
-                        goBackwards = false;
-                        _counterX++;
-                        _numCompleted++;
-                        _target = NumBoxes - 1 - _numCompleted;
-                    }
-                    else
-                    {
-                        _counterY--;
-                    }
-                }
-                else
-                {
-                    if (_counterY == _target)
-                    {
-                        doX = true;
-                        goBackwards = true;
-                        _counterX--;
-                        _target = _numCompleted;
-                    }
-                    else
-                    {
-                        _counterY++;
-                    }
-                }
-            }
+            SpiralTransitionLogic(bmpAddress, bmpWidth, bmpHeight, _counter++);
         }
     }
 }
