d4da23574e1cb98068f862533f042e87a1594f40
Kermalis <29823718+Kermalis@users.noreply.github.com>
2022-01-15 20:48:53 -0500

    Commit lost file

---
+++ PokemonGameEngine/Debug/Log.cs
@@ -0,0 +1,36 @@
+﻿#if DEBUG
+using System;
+
+namespace Kermalis.PokemonGameEngine.Debug
+{
+    internal static class Log
+    {
+        private const char IndentChar = '-';
+        private static int _indentLevel = 0;
+        private static string _indent = string.Empty;
+
+        public static void ModifyIndent(int levels)
+        {
+            if (levels == 0)
+            {
+                return;
+            }
+            _indentLevel += levels;
+            _indent = string.Empty.PadLeft(_indentLevel, IndentChar);
+        }
+
+        public static void WriteLine(string str)
+        {
+            if (_indentLevel != 0)
+            {
+                str = _indent + ' ' + str;
+            }
+            Console.WriteLine(str);
+        }
+        public static void WriteLineWithTime(string str)
+        {
+            WriteLine(string.Format("({0}) -- {1}", DateTime.Now.ToLongTimeString(), str));
+        }
+    }
+}
+#endif
