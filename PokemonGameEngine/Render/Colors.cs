13daafed7d46a70b7a15c48a18edd1aa1ccb1ff2
Kermalis <29823718+Kermalis@users.noreply.github.com>
2022-01-19 07:18:16 -0500

    Inline

--- PokemonGameEngine/Render/Colors.cs
+++ PokemonGameEngine/Render/Colors.cs
@@ -1,5 +1,6 @@
 ﻿using Silk.NET.OpenGL;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace Kermalis.PokemonGameEngine.Render
 {
@@ -14,27 +15,33 @@ namespace Kermalis.PokemonGameEngine.Render
         public static Vector4 Green4 { get; } = new Vector4(0, 1, 0, 1);
         public static Vector4 Blue4 { get; } = new Vector4(0, 0, 1, 1);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 FromRGB(uint r, uint g, uint b)
         {
             return new Vector3(r / 255f, g / 255f, b / 255f);
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector4 V4FromRGB(uint r, uint g, uint b)
         {
             return new Vector4(r / 255f, g / 255f, b / 255f, 1f);
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector4 FromRGBA(in Vector3 rgb, uint a)
         {
             return new Vector4(rgb, a / 255f);
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector4 FromRGBA(uint r, uint g, uint b, uint a)
         {
             return new Vector4(r / 255f, g / 255f, b / 255f, a / 255f);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void PutInShader(GL gl, int loc, Vector3 c)
         {
             gl.Uniform3(loc, 1, (float*)&c);
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void PutInShader(GL gl, int loc, Vector4 c)
         {
             gl.Uniform4(loc, 1, (float*)&c);
