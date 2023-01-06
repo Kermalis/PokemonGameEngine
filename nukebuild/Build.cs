218d949595c2af6cc0d7569a09eb2d73111ffe23
Kermalis <29823718+Kermalis@users.noreply.github.com>
2023-01-06 15:41:35 -0500

    Update nuke

--- nukebuild/Build.cs
+++ nukebuild/Build.cs
@@ -2,44 +2,41 @@
 using Nuke.Common.Execution;
 using Nuke.Common.IO;
 
-#pragma warning disable IDE0051 // Remove unused private members
-[CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 public sealed partial class Build : NukeBuild
 {
-    public static readonly AbsolutePath AssetPath = RootDirectory / "PokemonGameEngine" / "Assets";
+	public static readonly AbsolutePath AssetPath = RootDirectory / "PokemonGameEngine" / "Assets";
 
-    public static int Main()
-    {
-        return Execute<Build>(x => x.Compile);
-    }
+	public static int Main()
+	{
+		return Execute<Build>(x => x.Compile);
+	}
 
-    private Target Clean => _ => _
-    .Executes(CleanWorld, CleanScripts, CleanPokedata);
+	public Target Clean => _ => _
+	.Executes(CleanWorld, CleanScripts, CleanPokedata);
 
-    private Target Compile => _ => _
-    .After(Clean)
-    .Executes(BuildWorld, BuildScripts, BuildPokedata);
+	public Target Compile => _ => _
+	.After(Clean)
+	.Executes(BuildWorld, BuildScripts, BuildPokedata);
 
-    private Target CleanWorldOnly => _ => _
-    .Executes(CleanWorld);
+	public Target CleanWorldOnly => _ => _
+	.Executes(CleanWorld);
 
-    private Target CompileWorldOnly => _ => _
-    .After(CleanWorldOnly)
-    .Executes(BuildWorld);
+	public Target CompileWorldOnly => _ => _
+	.After(CleanWorldOnly)
+	.Executes(BuildWorld);
 
-    private Target CleanScriptsOnly => _ => _
-    .Executes(CleanScripts);
+	public Target CleanScriptsOnly => _ => _
+	.Executes(CleanScripts);
 
-    private Target CompileScriptsOnly => _ => _
-    .After(CleanScriptsOnly)
-    .Executes(BuildScripts);
+	public Target CompileScriptsOnly => _ => _
+	.After(CleanScriptsOnly)
+	.Executes(BuildScripts);
 
-    private Target CleanPokedataOnly => _ => _
-    .Executes(CleanPokedata);
+	public Target CleanPokedataOnly => _ => _
+	.Executes(CleanPokedata);
 
-    private Target CompilePokedataOnly => _ => _
-    .After(CleanPokedataOnly)
-    .Executes(BuildPokedata);
+	public Target CompilePokedataOnly => _ => _
+	.After(CleanPokedataOnly)
+	.Executes(BuildPokedata);
 }
-#pragma warning restore IDE0051 // Remove unused private members
